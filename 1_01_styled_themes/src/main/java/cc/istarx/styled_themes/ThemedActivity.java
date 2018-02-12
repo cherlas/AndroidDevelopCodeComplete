4248bd9dc95e10a6f62829fdafb0e442d639c594
cherlas <vm@li.cm>
2018-02-13 00:17:47 +0800

    themed state

--- 1_01_styled_themes/src/main/java/cc/istarx/styled_themes/ThemedActivity.java
+++ 1_01_styled_themes/src/main/java/cc/istarx/styled_themes/ThemedActivity.java
@@ -1,23 +1,23 @@
 package cc.istarx.styled_themes;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class StyledActivity extends Activity {
+public class ThemedActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_styled);
+        setContentView(R.layout.activity_themed);
 
-        findViewById(R.id.jumpAty).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.finish).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(StyledActivity.this, ThemedActivity.class));
+                finish();
             }
         });
+
     }
 }
