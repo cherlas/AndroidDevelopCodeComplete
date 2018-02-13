e608e123987cdad4e7f1a3e1eb4bc65afbf1adbb
cherlas <vm@li.cm>
2018-02-13 21:41:33 +0800

    hide navigation

--- 1_02_switchsystemui/src/main/java/cc/istarx/a1_02_switchsystemui/SwitchSystemUI.java
+++ 1_02_switchsystemui/src/main/java/cc/istarx/a1_02_switchsystemui/SwitchSystemUI.java
@@ -3,27 +3,42 @@ package cc.istarx.a1_02_switchsystemui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ToggleButton;
 
-public class SwitchSystemUI extends AppCompatActivity {
+public class SwitchSystemUI extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch_system_ui);
 
-        findViewById(R.id.toggleMode).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int currentVis = v.getSystemUiVisibility();
-                int newVis;
-                if ((currentVis & View.SYSTEM_UI_FLAG_LOW_PROFILE) == View.SYSTEM_UI_FLAG_LOW_PROFILE) {
-                    newVis = View.SYSTEM_UI_FLAG_VISIBLE;
-                } else {
-                    newVis = View.SYSTEM_UI_FLAG_LOW_PROFILE;
-                }
-                v.setSystemUiVisibility(newVis);
-            }
-        });
+        findViewById(R.id.toggle_system_ui).setOnClickListener(this);
+        findViewById(R.id.hide_navigation).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toggle_system_ui:
+                switchSystemUI(v);
+                break;
+            case R.id.hide_navigation:
+                hideNavigation(v);
+        }
+    }
+
+    private void hideNavigation(View view) {
+        // 只需要隐藏，隐藏之后点击屏幕任何位置都会显示出来导航栏
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    private void switchSystemUI(View view) {
+        int currentVis = view.getSystemUiVisibility();
+        int newVis;
+        if ((currentVis & View.SYSTEM_UI_FLAG_LOW_PROFILE) == View.SYSTEM_UI_FLAG_LOW_PROFILE) {
+            newVis = View.SYSTEM_UI_FLAG_VISIBLE;
+        } else {
+            newVis = View.SYSTEM_UI_FLAG_LOW_PROFILE;
+        }
+        view.setSystemUiVisibility(newVis);
     }
 }
