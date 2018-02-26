c032e6025df7bc131bc15db0dffa4a981cb269d2
cherlas <vm@li.cm>
2018-02-26 20:19:37 +0800

    硬币旋转

---
+++ 1_04_animatorview/src/main/java/cc/istarx/animatorview/FlipperActivity.java
@@ -0,0 +1,63 @@
+package cc.istarx.animatorview;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+/**
+ * Created by cherlas on 2018/2/26.
+ */
+
+public class FlipperActivity extends AppCompatActivity {
+
+    private boolean mIsHead;
+    private ObjectAnimator mFlipper;
+    private Bitmap mHeadImage;
+    private Bitmap mTailsImage;
+    private ImageView mFlipImage;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flipper);
+
+        mHeadImage = BitmapFactory.decodeResource(getResources(), R.drawable.heads);
+        mTailsImage = BitmapFactory.decodeResource(getResources(), R.drawable.tails);
+
+        mFlipImage = findViewById(R.id.flip_image);
+        mFlipImage.setImageBitmap(mHeadImage);
+        mIsHead = true;
+
+        mFlipper = ObjectAnimator.ofFloat(mFlipImage, "rotationY", 0f, 360f);
+        mFlipper.setDuration(500);
+        mFlipper.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (animation.getAnimatedFraction() > 0.25f && mIsHead) {
+                    mFlipImage.setImageBitmap(mTailsImage);
+                    mIsHead = false;
+                }
+                if (animation.getAnimatedFraction() > 0.75 && !mIsHead) {
+                    mFlipImage.setImageBitmap(mHeadImage);
+                    mIsHead =true;
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            mFlipper.start();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
--- 1_04_animatorview/src/main/java/cc/istarx/animatorview/MainActivity.java
+++ 1_04_animatorview/src/main/java/cc/istarx/animatorview/MainActivity.java
@@ -1,13 +1,15 @@
 package cc.istarx.animatorview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private View viewToAnimator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +19,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
 
         viewToAnimator = findViewById(R.id.the_view);
+
+        findViewById(R.id.complex_animator).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.complex_animator) {
+            startActivity(new Intent(MainActivity.this, FlipperActivity.class));
+        }
         if (viewToAnimator.getAlpha() > 0f) {
             viewToAnimator.animate().alpha(0f).translationX(1000f);
         } else {
